13d2a58dd0e285cec9b133651b0451246e0ecdc8
Enzo Sarmento <enzo.sarmento@youxgroup.com.br>
2022-07-25 17:01:28 -0300

    feat: class Position

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position position = new Position(7, 7);
+        System.out.println(position);
+    }
+}
